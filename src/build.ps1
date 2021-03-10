77c2bcba09fd7cb47f1cf8a7f01ce22cbb241c75
mimme <mirzamerdovic@gmail.com>
2021-03-10 20:11:45 +0100

    included build.ps1

---
+++ src/build.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = 'Stop'
+
+dotnet tool install --global dotnet-sonarscanner --version 5.1.0
+
+Set-Location -LiteralPath $PSScriptRoot
+
+$env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = '1'
+$env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
+$env:DOTNET_NOLOGO = '1'
+
+dotnet tool restore
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+dotnet cake @args
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
