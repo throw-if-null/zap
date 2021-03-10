3a912b4b1d2762097800a2e851c1bdc36d7cc2fa
mimme <mirzamerdovic@gmail.com>
2021-03-10 20:19:15 +0100

    fixes

--- src/build.ps1
+++ src/build.ps1
@@ -8,6 +8,8 @@ $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = '1'
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
 $env:DOTNET_NOLOGO = '1'
 
+dotnet tool install --global Cake.Tool --version 1.1.0
+
 dotnet tool restore
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
