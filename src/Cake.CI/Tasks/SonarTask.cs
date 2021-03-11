bb2dc58a402588324432d3a8c057a13f675b4798
mimme <mirzamerdovic@gmail.com>
2021-03-12 00:23:26 +0100

    ss

--- src/Cake.CI/Tasks/SonarTask.cs
+++ src/Cake.CI/Tasks/SonarTask.cs
@@ -37,9 +37,10 @@ namespace Cake.CI.Tasks
                     ["sonar.links.issue"] = "https://github.com/MirzaMerdovic/zap/issues",
                     ["sonar.links.scm="] = "https://github.com/MirzaMerdovic/zap",
 
-                    ["sonar.inclusions"] = "**/*.cs",
+                    //["sonar.inclusions"] = "**/*.cs",
+                    ["sonar.exclusions"] = "**\\bin\\**\\*.cs, **\\obj\\**\\*.cs",
                     ["sonar.cs.opencover.reportsPaths"] = "reports/*/coverage.opencover.xml",
-                    ["sonar.coverage.exclusions"] = @"**Test*.cs, **\bin\**\*.*, **\obj\**\*.*"
+                    ["sonar.coverage.exclusions"] = @"**Test*.cs"
                 }
             };
 
